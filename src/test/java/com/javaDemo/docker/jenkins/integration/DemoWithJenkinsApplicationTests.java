ea3d4bdee76099e4a26a9aef44807b8b8c184e79
Manish-at-057023364035 <manishgupta0690@gmail.com>
2020-06-02 18:14:12 +0530

    commit project

---
+++ src/test/java/com/javaDemo/docker/jenkins/integration/DemoWithJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaDemo.docker.jenkins.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWithJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
